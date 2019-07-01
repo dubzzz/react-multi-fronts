4407b940897e852bd13a7f3d421aff78a0ffc8eb
Nicolas DUBIEN <github@dubien.org>
2019-07-02 00:35:13 +0200

    Add a nice menu

--- app-main/src/App.tsx
+++ app-main/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AppTeamA = React.lazy(() =>
   //@ts-ignore
@@ -10,14 +10,50 @@ const AppTeamB = React.lazy(() =>
 );
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
+  const [page, setPage] = useState<"home" | "app-a" | "app-b">("home");
+  const [history, setHistory] = useState([] as string[]);
+
+  const CurrentApp =
+    page === "home" ? (
+      <h2>Welcome to the home page</h2>
+    ) : page === "app-a" ? (
       <React.Suspense fallback={<div>Loading App A...</div>}>
         <AppTeamA />
       </React.Suspense>
+    ) : (
       <React.Suspense fallback={<div>Loading App B...</div>}>
-        <AppTeamB history={["user.a", "user.b"]} />
+        <AppTeamB history={history} />
       </React.Suspense>
+    );
+  return (
+    <div>
+      <div className="Menu">
+        <button
+          onClick={() => {
+            setPage("home");
+            setHistory([...history, "Go to home"]);
+          }}
+        >
+          Home
+        </button>
+        <button
+          onClick={() => {
+            setPage("app-a");
+            setHistory([...history, "Go to App A"]);
+          }}
+        >
+          App A
+        </button>
+        <button
+          onClick={() => {
+            setPage("app-b");
+            setHistory([...history, "Go to App B"]);
+          }}
+        >
+          App B
+        </button>
+      </div>
+      <div className="App">{CurrentApp}</div>
     </div>
   );
 };
