85c826c04eb0cd61376e57b47dbfbc5f7b7145e8
Nicolas DUBIEN <github@dubien.org>
2019-07-02 00:27:25 +0200

    Add a dependency to an external package

--- app-team-a/src/App.tsx
+++ app-team-a/src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import * as fc from "fast-check";
 
 const App: React.FC = () => {
-  return <h2>Welcome to the App of Team A</h2>;
+  return (
+    <p>
+      <h2>Welcome to the App of Team A</h2>
+      <p>{fc.sample(fc.lorem(10, true))[0]}</p>
+    </p>
+  );
 };
 
 export default App;
